855fe666ef78d79344c561f7af8b793eb4d49baa
Luis Vega <electromummyfied@gmail.com>
2023-09-21 09:46:39 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include 'header.php' ?>
+
+<!-- <h1>Path to Graduation</h1> -->
+
+<!-- <pre><code class="language-cpp">
+<?php include 'main.cxx' ?>
+</code></pre> -->
+
+Appendix A <br><br>
+
+Definite Integral
+$$<?php include 'latex/equations/continuous-domain/integral.tex' ?>$$
+
+Inner-Product
+$$<?php include 'latex/equations/continuous-domain/inner-product.tex' ?>$$
+
+L2-Norm
+$$<?php include 'latex/equations/continuous-domain/l2-norm.tex' ?>$$
+
+Inner-Product Transform
+$$<?php include 'latex/equations/continuous-domain/inner-product-transform.tex' ?>$$
+
+Bilateral Laplace Transform
+$$<?php include 'latex/equations/continuous-domain/bilateral-laplace-transform.tex' ?>$$
+
+Laplace Transform
+$$<?php include 'latex/equations/continuous-domain/laplace-transform.tex' ?>$$
+
+Fourier Transform
+$$<?php include 'latex/equations/continuous-domain/fourier-transform.tex' ?>$$
+
+Wavelet Transform
+$$<?php include 'latex/equations/continuous-domain/wavelet-transform.tex' ?>$$
+
+
+
+<?php include 'footer.php' ?>
